a4f177fca358815d5e2bcc74179ea93f048f7777
jslopeza <jasonlopeza95@gmail.com>
2015-03-06 14:03:52 -0500

    Initial Setup

---
+++ server.js
@@ -0,0 +1,39 @@
+// Basic Setup
+// ===========
+
+// Get the packages
+var express = require('express'),
+	app = express(),
+	bodyParser = require('body-parser'),
+	morgan = require('morgan'),
+	mongoose = require('mongoose'),
+	port = process.env.PORT || 8080;
+
+// App Config
+
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+
+app.use(function(req, res, next){
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods','GET, POST');
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, \Authorization');
+	next();
+});
+
+app.use(morgan('dev'));
+
+app.get('/', function(req, res){
+	res.send('Welcome to the home page!');
+});
+
+var apiRouter = express.Router();
+
+apiRouter.get('/', function(req, res){
+	res.json({message : 'hooray! welcome to our api!'});
+});
+
+app.use('/api', apiRouter);
+
+app.listen(port);
+console.log('Magic happens on port ' + port);
