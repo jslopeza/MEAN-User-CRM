8e1f525bec57aa3efc7d7109377e0d75bf129a98
jslopeza <jasonlopeza95@gmail.com>
2015-03-17 14:13:48 -0400

    Fixed code error

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ apiRouter.use(function(req, res, next) {
     var token = req.body.token || req.param('token') || req.headers['x-access-token'];
 
     if (token) {
-        jwt.verify(token, superSecret, function(err, decoded) {
+        jwt.verify(token, config.secret, function(err, decoded) {
             if (err) {
                 return res.status(403).send({
                     success: false,
