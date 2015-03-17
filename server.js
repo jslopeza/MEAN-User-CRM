25419481f8df036e9c11e2b0ab5934ed88c8e1da
jslopeza <jasonlopeza95@gmail.com>
2015-03-17 14:06:46 -0400

    Add configurations to seprate file

--- server.js
+++ server.js
@@ -3,104 +3,117 @@
 
 // Get the packages
 var express = require('express'),
-	app = express(),
-	bodyParser = require('body-parser'),
-	morgan = require('morgan'),
-	mongoose = require('mongoose'),
-	port = process.env.PORT || 8080,
-	User = require('./app/models/user');
+    app = express(),
+    bodyParser = require('body-parser'),
+    morgan = require('morgan'),
+    mongoose = require('mongoose'),
+    User = require('./app/models/user'),
+    config = require('./config');
 
-mongoose.connect('mongodb://localhost:27107/mean-machine');
+mongoose.connect(config.database);
 
 // App Config
 
-app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(bodyParser.json());
 
-app.use(function(req, res, next){
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Allow-Methods','GET, POST');
-	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, Authorization');
-	next();
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, Authorization');
+    next();
 });
 
 app.use(morgan('dev'));
 
-app.get('/', function(req, res){
-	res.send('Welcome to the home page!');
+app.get('/', function(req, res) {
+    res.send('Welcome to the home page!');
 });
 
 var apiRouter = express.Router();
 
-apiRouter.use(function(req, res, next){
-	console.log('Somebody just came to our app!');
-	next();
+apiRouter.use(function(req, res, next) {
+    console.log('Somebody just came to our app!');
+    next();
 });
 
-apiRouter.get('/', function(req, res){
-	res.json({message : 'hooray! welcome to our api!'});
+apiRouter.get('/', function(req, res) {
+    res.json({
+        message: 'hooray! welcome to our api!'
+    });
 });
 
 apiRouter.route('/users')
-	.post(function(req, res){
-		var user = new User();
-		user.name = req.body.name;
-		user.username = req.body.username;
-		user.password = req.body.password;
+    .post(function(req, res) {
+        var user = new User();
+        user.name = req.body.name;
+        user.username = req.body.username;
+        user.password = req.body.password;
 
-		user.save(function(err){
-			if(err){
-				if(err.code === 11000){
-					return res.json({sucess : false, message : 'A user with that username already exists.'});
-				} else {
-					return res.send(err);
-				}
-			}
+        user.save(function(err) {
+            if (err) {
+                if (err.code === 11000) {
+                    return res.json({
+                        sucess: false,
+                        message: 'A user with that username already exists.'
+                    });
+                } else {
+                    return res.send(err);
+                }
+            }
 
-			res.json({message : 'User created!'});
-		});
-	})
-	.get(function(req, res){
-		User.find(function(err, users){
-			if(err) res.send(err);
+            res.json({
+                message: 'User created!'
+            });
+        });
+    })
+    .get(function(req, res) {
+        User.find(function(err, users) {
+            if (err) res.send(err);
 
-			res.json(users);
-		});
-	});
+            res.json(users);
+        });
+    });
 
 apiRouter.route('/users/:user_id')
-	.get(function(req, res){
-		User.findById(req.params.user_id, function(err, user){
-			if(err) res.send(err);
+    .get(function(req, res) {
+        User.findById(req.params.user_id, function(err, user) {
+            if (err) res.send(err);
 
-			res.json(user);
-		});
-	})
-	.put(function(req, res){
-		User.findById(req.params.user_id, function(err, user){
-			if(err) res.send(err);
+            res.json(user);
+        });
+    })
+    .put(function(req, res) {
+        User.findById(req.params.user_id, function(err, user) {
+            if (err) res.send(err);
 
-			if(req.body.name) user.name = req.body.name;
-			if(req.body.username) user.username = req.body.username;
-			if(req.body.password) user.password = req.body.password;
+            if (req.body.name) user.name = req.body.name;
+            if (req.body.username) user.username = req.body.username;
+            if (req.body.password) user.password = req.body.password;
 
-			user.save(function(err){
-				if(err) res.send(err);
+            user.save(function(err) {
+                if (err) res.send(err);
 
-				res.json({message : 'User updated!'});
-			});
-		});
-	})
-	.delete(function(req, res){
-		User.remove({
-			_id : req.params.user_id
-		}, function(err){
-			if(err) return res.send(err);
+                res.json({
+                    message: 'User updated!'
+                });
+            });
+        });
+    })
+    .delete(function(req, res) {
+        User.remove({
+            _id: req.params.user_id
+        }, function(err) {
+            if (err) return res.send(err);
 
-			res.json({message : 'Successfully deleted'});
-		});
-	});
+            res.json({
+                message: 'Successfully deleted'
+            });
+        });
+    });
 app.use('/api', apiRouter);
 
-app.listen(port);
-console.log('Magic happens on port ' + port);
+app.listen(config.port);
+console.log('Magic happens on port ' + config.port);
